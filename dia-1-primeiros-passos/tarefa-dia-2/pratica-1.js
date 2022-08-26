fcadac4df96122f949dd7c7eb4e0d025688093a6
Catarina_zakka <cahzakka@gmail.com>
2022-08-26 01:11:15 -0300

    tarefa-dia-2-ok-alterado

--- dia-1-primeiros-passos/tarefa-dia-2/pratica-1.js
+++ dia-1-primeiros-passos/tarefa-dia-2/pratica-1.js
@@ -1,6 +1,11 @@
 // Variavel nome; idade e etc; 
-let nome 
-let email
-let profissao
-let idade
-let viagem
+let nome = "Catarina"
+console.log(nome)
+let email = "cahzakka@gmail.com"
+console.log(email)
+let profissao = "marketing"
+console.log(profissao)
+let idade = "22"
+console.log(idade)
+let viagem = "Roma"
+console.log(viagem)
