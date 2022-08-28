364c1eb3241ae0e50ad88b77dc800a55821aa4c6
Catarina_zakka <cahzakka@gmail.com>
2022-08-28 14:50:02 -0300

    tarefa-dia-3-repetição-ok

---
+++ dia-1-primeiros-passos/tarefa-dia-3/pratica-1.js
@@ -0,0 +1,13 @@
+//Implementando meu primeiro comando de repetição
+
+//Considere que o dado de entrada para executar o código é o número n. Ou seja, uma variável n(n será  igual á 15) que vai armazenar o valor do número final que queremos exibir
+
+// Variavel 
+let n = 15
+
+// Código do loop
+
+while (n <= 30) {
+    console.log(n)
+    n = n + 1
+}
