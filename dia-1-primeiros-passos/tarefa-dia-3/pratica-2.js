b6be25393f644b9596c12555f4c35dc0fe35ff2c
Catarina_zakka <cahzakka@gmail.com>
2022-08-28 17:07:18 -0300

    Tabuada-1-ok

---
+++ dia-1-primeiros-passos/tarefa-dia-3/pratica-2.js
@@ -0,0 +1,16 @@
+//Prática 2: Fazendo uma tabuada
+
+//Tabuada do 1  
+
+// Variavel
+
+let x = 1 
+let y = 10 
+let z = 1
+
+while(z <= y) {
+    let resultado = x * z
+    let soma = `${x} x ${z} = ${resultado}`
+    console.log (soma)
+    z++
+}
